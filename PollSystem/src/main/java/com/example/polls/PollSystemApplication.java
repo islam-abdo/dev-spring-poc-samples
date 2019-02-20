bd231aee87e109aab44da0faadf9c50dce98af37
Islam Abdo <Islam Abdo@DESKTOP-FJEP8D7.mshome.net>
2019-02-20 14:40:22 +0200

    Adding PollSystem Project

---
+++ PollSystem/src/main/java/com/example/polls/PollSystemApplication.java
@@ -0,0 +1,32 @@
+package com.example.polls;
+
+import java.util.TimeZone;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@SpringBootApplication
+@EntityScan(basePackageClasses = { 
+		PollSystemApplication.class,
+		Jsr310JpaConverters.class 
+})
+public class PollSystemApplication {
+	@PostConstruct
+	void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(PollSystemApplication.class, args);
+	}
+
+}
+
